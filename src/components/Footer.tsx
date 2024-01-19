747a588cb115eb4cc1bd1a4f6701a21e606bf2ba
Salvador <salvador.karakachoff@gmail.com>
2024-01-18 23:52:42 -0300

    Update footer text to use English language

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
             </div>
           </a>
           <p className="">© 2024 <a href="https://www.linkedin.com/in/salvador-karakachoff/" className="relative bg-gradient-to-r from-violet-400 to-purple-500 bg-clip-text text-transparent decoration-purple-500 decoration-2 transition-all after:absolute after:-bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-gradient-to-r after:from-violet-400 after:to-purple-500 after:opacity-80 after:transition-transform after:duration-500 after:ease-spring hover:after:origin-bottom-left hover:after:scale-x-100">Salvador Karakachoff</a></p>
-          <p className="">Utilizando Next.js, React, Typescript, Tailwind CSS y la API de Co:Here</p>
+          <p className="">Using Next.js, React, Typescript, Tailwind CSS & Co:Here&apos;s API</p>
       </footer>
     );
   }
